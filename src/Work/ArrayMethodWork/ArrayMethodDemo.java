0b502b6475a92ea434d70be068a89b0f43e719b8
GageWaack <45102148+GageWaack@users.noreply.github.com>
2019-10-04 09:47:46 -0500

    bn

--- src/Work/ArrayMethodWork/ArrayMethodDemo.java
+++ src/Work/ArrayMethodWork/ArrayMethodDemo.java
@@ -1,10 +1,9 @@
 package Work.ArrayMethodWork;
 
 public class ArrayMethodDemo
-{
-    public static void main (String [] args)
+{    public static void main (String [] args)
     {
-        int[] omega = {1,2,3,4,5};
+        int[] omega = {1,2,3,4,5,6,7,8,9,10};
         displayForward(omega);
        displayreverse(omega);
         displaysum(omega);
@@ -12,23 +11,23 @@ public class ArrayMethodDemo
     public static void displayForward(int[] omega){
 
         for (int j = 0; j < omega.length; j++ ){
-            System.out.println(omega[j]);
+            System.out.println("Numbers given are "+ omega[j]);
         }
     }
     public static void displayreverse(int[] omega)
     {
 
         for(int i = omega.length-1; i >= 0; --i) {
-            System.out.println(omega[i]);
+            System.out.println("Numbers backwards is "+ omega[i]);
         }
     }
        public static void displaysum(int[] omega)
     {
-        for (int total = omega.length; --total)
+        int sum = 0;
+        for (int value : omega)
         {
-            total = total + omega;
-            System.out.println("Here is the sum " + total);
+        sum += value;
+        System.out.println("The sum is "+ sum);
         }
-
     }
 }
